8c3268b7d0e28cac3e941ea9bdab0f7c25c1f06e
Jan Borgersen <jborgersen@gmail.com>
2017-11-19 16:26:04 -0800

    formatting.

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,17 @@
 /**
- *	ics2json Google Cloud Function
- *
- *  Copyright (c) 2017 Jan Martin Borgersen
- *
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- */
+*	ics2json Google Cloud Function
+*
+*  Copyright (c) 2017 Jan Martin Borgersen
+*
+*  MIT License
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*/
 
 'use strict';
 
@@ -36,79 +36,79 @@ var ics2json_function = function (req, res)
     // returns a sorted array of VEVENTS with startdate property in ms
     function sortEventsByDate(dataObj)
     {
-    var arr = [];
-    for (var k in dataObj)
-    {
-    if (dataObj.hasOwnProperty(k) &&
-    dataObj[k].hasOwnProperty("type") &&
-    dataObj[k]["type"] === "VEVENT")
-    {
-    arr.push({
-    "key":k,
-    "data":dataObj[k],
-    "startdate":(new Date(dataObj[k]["start"])).getTime()
-    });
-    }
-    }
-    arr.sort(function(a,b) {
-    if (a.startdate < b.startdate)
-          return -1;
-        if (a.startdate > b.startdate)
-          return 1;
-        return 0;
-    });
-    return arr;
+        var arr = [];
+        for (var k in dataObj)
+        {
+            if (dataObj.hasOwnProperty(k) &&
+                dataObj[k].hasOwnProperty("type") &&
+                dataObj[k]["type"] === "VEVENT")
+            {
+                arr.push({
+                    "key":k,
+                    "data":dataObj[k],
+                    "startdate":(new Date(dataObj[k]["start"])).getTime()
+                });
+            }
+        }
+        arr.sort(function(a,b) {
+            if (a.startdate < b.startdate)
+                return -1;
+            if (a.startdate > b.startdate)
+                return 1;
+            return 0;
+        });
+        return arr;
     }
 
     if (!icsUrl)
     {
-    var usageObj = {
-    "name":"ics2json",
-    "GET parameters":
-    {
-    "ics":"url to query",
-    "filter":"'future', 'past'",
-    "count":"number to return. default is 100."
-    }
-    };
+        var usageObj = {
+            "name":"ics2json",
+            "GET parameters":
+            {
+                "ics":"url to query",
+                "filter":"'future', 'past'",
+                "count":"number to return. default is 100."
+            }
+        };
 
-    res.status(200).send(JSON.stringify({"usage":usageObj}));
+        res.status(200).send(JSON.stringify({"usage":usageObj}));
     }
     else
     {
-    ical.fromURL(icsUrl, {}, function(err, data)
-    {
-    if (err)
-    {
-    res.status(500).send(JSON.stringify({"error":err}));
-    }
-    else
-    {
-    var ret = {};
-    var eventsArr = sortEventsByDate(data);
-    var numEvents = eventsArr.length;
-    var filteredEventsArr = [];
+        ical.fromURL(icsUrl, {}, function(err, data)
+        {
+            if (err)
+            {
+                res.status(500).send(JSON.stringify({"error":err}));
+            }
+            else
+            {
+                var ret = {};
+                var eventsArr = sortEventsByDate(data);
+                var numEvents = eventsArr.length;
+                var filteredEventsArr = [];
 
-    switch(filter)
-    {
-    case "future":
-    filteredEventsArr = eventsArr
-    .filter((a) => a.startdate > now)
-    .slice(0,count);
-    break;
-    case "past":
-    filteredEventsArr = eventsArr.filter((a) => a.startdate < now);
-    filteredEventsArr = filteredEventsArr.slice(filteredEventsArr.length - count, filteredEventsArr.length);
-    break;
-    default:
-    filteredEventsArr = eventsArr.slice(0,count);
-    }
+                switch(filter)
+                {
+                    case "future":
+                        filteredEventsArr = eventsArr
+                        .filter((a) => a.startdate > now)
+                        .slice(0,count);
+                        break;
+                    case "past":
+                        filteredEventsArr = eventsArr.filter((a) => a.startdate < now);
+                        filteredEventsArr = filteredEventsArr.slice(filteredEventsArr.length - count, filteredEventsArr.length);
+                        break;
+                    default:
+                        filteredEventsArr = eventsArr.slice(0,count);
+                }
 
-    ret["data"] = filteredEventsArr;
+                ret["data"] = filteredEventsArr;
 
-    res.status(200).send(JSON.stringify(ret));
-    }
-    });
+                res.status(200).send(JSON.stringify(ret));
+            }
+        });
     }
 };
 
@@ -117,6 +117,6 @@ exports.ics2json = function ics2json(req, res)
     var corsFn = cors(corsOptions);
     corsFn(req, res, function()
     {
-    ics2json_function(req, res);
+        ics2json_function(req, res);
     });
 }
